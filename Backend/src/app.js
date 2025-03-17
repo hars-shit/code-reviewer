233645812398c7e5cdd8e8ab6a0f6f5e1b46f713
harshit <upadhyayharshit05@gmail.com>
2025-03-17 12:11:53 +0530

    updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,13 @@ const cors=require('cors')
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+   {
+      origin: "*", // or specify a domain
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"]
+   }
+))
 
 
 app.get('/',(req,res)=>{
